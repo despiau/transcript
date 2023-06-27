e078ef45518cd5fe6d120f3d5b6d0e81f02f097f
Luis Despiau <30500498+despiau@users.noreply.github.com>
2023-06-27 16:25:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
-const chatbox = document.querySelector('.chatbox');
+const chatbox = document.querySelector('#chat-box');
 const saveButton = document.querySelector('.save-btn');
-const micButton = document.querySelector('.mic-btn');
+const micButton = document.querySelector('#mic-button');
 const recognition = new window.webkitSpeechRecognition();
-const liveTranscriptElement = document.querySelector('.live-transcript p'); 
-const noteInput = document.querySelector('.note-input input');
-const addNoteButton = document.querySelector('.note-input button');
+const liveTranscriptElement = document.querySelector('#live-transcript'); 
+const noteInput = document.querySelector('#input');
+const addNoteButton = document.querySelector('#submit-button');
 
 let isRecording = false;
 let currentTranscript = '';
 
-let mediaRecorder;
-let chunks = [];
-
-navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(function(stream) {
-    mediaRecorder = new MediaRecorder(stream);
-    
-    mediaRecorder.ondataavailable = function(e) {
-      chunks.push(e.data);
-    };
-
-    mediaRecorder.onstop = function() {
-      let blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
-      chunks = [];
-      let audioURL = window.URL.createObjectURL(blob);
-      // You might want to add a way for the user to download the recorded audio
-      // or you could save it to a server.
-      console.log(audioURL);
-    };
-});
-
-micButton.addEventListener('click', () => {
+micButton.addEventListener('click', (event) => {
+  event.preventDefault();
   if (!isRecording) {
     recognition.continuous = true;
     recognition.interimResults = true;
@@ -39,17 +19,11 @@ micButton.addEventListener('click', () => {
     recognition.start();
     micButton.classList.add('recording');
     micButton.innerHTML = '🔴';
-    
-    // Start mediaRecorder
-    mediaRecorder.start();
   } else {
     recognition.stop();
     micButton.classList.remove('recording');
     micButton.innerHTML = '🎤';
     recognition.removeEventListener('result', handleRecognitionResult);
-    
-    // Stop mediaRecorder
-    mediaRecorder.stop();
   }
   isRecording = !isRecording;
 });
@@ -65,6 +39,7 @@ function handleRecognitionResult(event) {
   const lastResult = results[results.length - 1];
   const transcript = lastResult[0].transcript.trim();
   updateLiveTranscript(transcript);
+
   if (lastResult.isFinal) {
     const message = formatMessage(transcript);
     if (message) {
@@ -95,70 +70,22 @@ let previousMessage = '';
 function addMessage(message, speaker) {
   if (message !== previousMessage) {
     const messageElement = document.createElement('div');
-    messageElement.classList.add('message');
-    messageElement.classList.add(speaker.toLowerCase());
-    const messageBubbleElement = document.createElement('div');
-    messageBubbleElement.classList.add('message-bubble');
-    const messageTextElement = document.createElement('p');
-    messageTextElement.innerText = message;
-    const timestampElement = document.createElement('div');
-    timestampElement.classList.add('timestamp');
-    timestampElement.innerText = getTimeStamp();
-    messageBubbleElement.appendChild(messageTextElement);
-    messageBubbleElement.appendChild(timestampElement);
-    messageElement.appendChild(messageBubbleElement);
+    messageElement.classList.add('list-group-item');
+    messageElement.innerText = `${speaker}: ${message}`;
     chatbox.appendChild(messageElement);
     previousMessage = message;
   }
 }
 
 function scrollChatboxToBottom() {
-  setTimeout(() => {
-    chatbox.scrollTop = chatbox.scrollHeight;
-  }, 100);
+  chatbox.scrollTop = chatbox.scrollHeight;
 }
 
-function saveTranscript() {
-  const messages = chatbox.querySelectorAll('.message');
-  let transcript = '';
-  messages.forEach((message) => {
-    const messageTextElement = message.querySelector('p');
-    const timestampElement = message.querySelector('.timestamp');
-    const speakerElement = message.classList.contains('speaker') ? 'Speaker' : (message.classList.contains('note') ? 'Note' : 'Unknown');
-    const messageText = messageTextElement.innerText;
-    const timestamp = timestampElement.innerText;
-    transcript += `[${timestamp}] [${speakerElement}] ${messageText}\n`;
-  });
-  const element = document.createElement('a');
-  const file = new Blob([transcript], {type: 'text/plain'});
-  element.href = URL.createObjectURL(file);
-  element.download = 'transcript.txt';
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
-}
-
-saveButton.addEventListener('click', () => {
-  saveTranscript();
-});
-
-function getTimeStamp() {
-  const date = new Date();
-  const hours = date.getHours() % 12 || 12;
-  const minutes = addZeroPadding(date.getMinutes());
-  const seconds = addZeroPadding(date.getSeconds());
-  const amOrPm = date.getHours() >= 12 ? 'pm' : 'am';
-  return `${hours}:${minutes}:${seconds} ${amOrPm}`;
-}
-
-function addZeroPadding(number) {
-  return number < 10 ? `0${number}` : `${number}`;
-}
-
-addNoteButton.addEventListener('click', () => {
+addNoteButton.addEventListener('click', (event) => {
+  event.preventDefault();
   const noteText = noteInput.value.trim();
   if (noteText) {
-    addMessage(noteText, 'Note');
+    addMessage(noteText, 'User');
     scrollChatboxToBottom();
     noteInput.value = '';
   }
