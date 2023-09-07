0348d5df3f12264d9cf44fd8e01ebeafd6adf2f3
Luis Despiau <30500498+despiau@users.noreply.github.com>
2023-09-07 10:27:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// script.js
-
+// Get references to DOM elements
 const chatbox = document.querySelector('#chat-box');
 const saveButton = document.querySelector('#save-button');
 const saveCleanButton = document.querySelector('#save-clean-button');
@@ -9,14 +8,14 @@ const recognition = new window.webkitSpeechRecognition();
 const liveTranscriptElement = document.querySelector('#live-transcript');
 const noteInput = document.querySelector('#input');
 const addNoteButton = document.querySelector('#submit-button');
-const scrollBottomButton = document.querySelector('#scroll-bottom-btn');
 const hamburgerMenuButton = document.querySelector('.hamburger-menu');
 const sidebar = document.querySelector('.sidebar');
 const headerButtons = document.querySelector('.header-buttons');
 
+// Initialize variables
 let isRecording = false;
-let currentTranscript = '';
 
+// Event listener for the microphone button
 micButton.addEventListener('click', (event) => {
   event.preventDefault();
   if (!isRecording) {
@@ -36,12 +35,14 @@ micButton.addEventListener('click', (event) => {
   isRecording = !isRecording;
 });
 
+// Event listener for speech recognition end event
 recognition.addEventListener('end', () => {
   if (isRecording) {
     recognition.start();
   }
 });
 
+// Function to handle speech recognition results
 function handleRecognitionResult(event) {
   const results = event.results;
   const lastResult = results[results.length - 1];
@@ -58,10 +59,12 @@ function handleRecognitionResult(event) {
   }
 }
 
+// Function to update live transcript
 function updateLiveTranscript(transcript) {
   liveTranscriptElement.innerText = transcript;
 }
 
+// Function to format a message
 function formatMessage(message) {
   const words = message.split(' ');
   const uniqueWords = [...new Set(words)];
@@ -73,6 +76,7 @@ function formatMessage(message) {
   return uniqueWords.join(' ');
 }
 
+// Function to add a message to chatbox
 function addMessage(message, speaker) {
   const timeStamp = new Date().toLocaleTimeString();
   const messageElement = document.createElement('div');
@@ -85,10 +89,12 @@ function addMessage(message, speaker) {
   scrollChatboxToBottom();
 }
 
+// Function to scroll chatbox to the bottom
 function scrollChatboxToBottom() {
   chatbox.scrollTop = chatbox.scrollHeight;
 }
 
+// Event listener for adding a note
 addNoteButton.addEventListener('click', (event) => {
   event.preventDefault();
   const noteText = noteInput.value.trim();
@@ -99,6 +105,7 @@ addNoteButton.addEventListener('click', (event) => {
   }
 });
 
+// Event listener for Enter key to add a note
 noteInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter' && !event.shiftKey) {
     event.preventDefault();
@@ -111,11 +118,13 @@ noteInput.addEventListener('keydown', (event) => {
   }
 });
 
+// Function to save the transcript
 function saveTranscript() {
   const date = new Date().toLocaleDateString();
   const meetingTitle = `Transcription from Meeting [${date}]`;
   let fullTranscript = `${meetingTitle}\n\n`;
-  let cleanTranscript = `${meetingTitle}\n\n`;
+  // Initialize cleanTranscript with the prompt
+  let cleanTranscript = `${meetingTitle}\n\nPrompt: Write a bullet point summary and action item list.\n\n`;
 
   chatbox.childNodes.forEach((node) => {
     const speaker = node.querySelector('strong').textContent;
@@ -126,7 +135,7 @@ function saveTranscript() {
   });
 
   const totalCharacters = fullTranscript.length;
-  const totalWords = fullTranscript.split(/\s+/).length - 1; // Exclude line breaks
+  const totalWords = fullTranscript.split(/\s+/).length - 1;
 
   fullTranscript += `\nTotal Characters: ${totalCharacters}\n`;
   fullTranscript += `Total Words: ${totalWords}\n`;
@@ -137,6 +146,7 @@ function saveTranscript() {
   };
 }
 
+// Event listener for saving the full transcript
 saveButton.addEventListener('click', () => {
   const transcripts = saveTranscript();
   const blob = new Blob([transcripts.fullTranscript], { type: 'text/plain;charset=utf-8' });
@@ -147,6 +157,7 @@ saveButton.addEventListener('click', () => {
   link.click();
 });
 
+// Event listener for saving the clean transcript
 saveCleanButton.addEventListener('click', () => {
   const transcripts = saveTranscript();
   const blob = new Blob([transcripts.cleanTranscript], { type: 'text/plain;charset=utf-8' });
@@ -157,6 +168,7 @@ saveCleanButton.addEventListener('click', () => {
   link.click();
 });
 
+// Event listener for the hamburger menu
 hamburgerMenuButton.addEventListener('click', () => {
   sidebar.classList.toggle('sidebar-open');
   headerButtons.classList.toggle('header-buttons-open');
